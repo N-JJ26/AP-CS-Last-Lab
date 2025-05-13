41088b618221b9f609c0b880a658b3236c19d0a3
N-JJ26 <nathanielj613@gmail.com>
2025-05-13 11:47:15 -0400

    finishing up day 1 of actual programming

--- src/Animal/Consumers/ArcticFox.java
+++ src/Animal/Consumers/ArcticFox.java
@@ -1,5 +1,38 @@
 package src.Animal.Consumers;
 
-public class ArcticFox {
-    
+import src.Animal.Animal;
+import src.Animal.Carnivore;
+import src.Animal.Herbivore;
+import src.Plants.*;
+
+public class ArcticFox extends Animal implements Carnivore, Herbivore {
+    /*TODO:
+     * finish up code
+     */
+
+    public ArcticFox()
+    {
+        super( 50 + ( int )( Math.random() * 7 ), 5200, 14 + ( int )( Math.random() * 11 ),
+                304, 16.3);
+    }
+
+    public boolean isHungry()
+    {
+        return false;
+    }
+
+    public void eat( Animal a )
+    {
+
+    }
+
+    public void eat( Plant p )
+    {
+        
+    }
+
+    public boolean wasHuntSuccessful()
+    {
+        return false;
+    }
 }
