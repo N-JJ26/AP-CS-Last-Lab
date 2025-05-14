7ae6b01d1ab6d1b4502575409acb15550ee6f8b2
austinbenedicto <austinbenedicto271@gmail.com>
2025-05-14 08:40:33 -0400

    worked on Caribou need to implement more stuff

--- src/Animal/Consumers/Caribou.java
+++ src/Animal/Consumers/Caribou.java
@@ -1,5 +1,37 @@
 package src.Animal.Consumers;
+import src.Animal.Animal;
+import src.Animal.Herbivore;
+import src.Plants.Plant;
 
-public class Caribou {
+/**
+ * Caribou class represents a herbivorous animal that can be found in the Arctic region.
+ * It extends the Animal class and implements the Herbivore interface.
+ * 
+ * @author Austin Benedicto
+ * @version 5/14/2025
+ */
+public class Caribou extends Animal implements Herbivore
+{
+    private static final int BODY_MASS_NEW_BORN = 6500;
+    private static final int BODY_MASS_ADULT = 101250;
+    private static final int WEANING_AGE = 120;
+    private static final int ONSET_FERT_MALE = 680;
+    private static final double MAX_LIFE_SPAN = 14.0; // in years
+
+    public Caribou()
+    {
+        super(BODY_MASS_NEW_BORN, BODY_MASS_ADULT, WEANING_AGE, ONSET_FERT_MALE, MAX_LIFE_SPAN);
+    }
+
+    public void eat(Plant p)
+    {
+        //TODO: Implement the logic for eating a plant
+    }
+
+    public boolean isHungry()
+    {
+        //TODO: Implement the logic to determine if the Caribou is hungry
+        return true;
+    }
     
 }
