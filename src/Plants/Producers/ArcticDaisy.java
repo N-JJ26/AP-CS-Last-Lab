43ec8cf3fd35f25c718b576f5e2a492d92bc4d1d
Avi-Dasgupta <avidasgupta7@icloud.com>
2025-05-14 08:39:37 -0400

    finished ArcticDaisy

--- src/Plants/Producers/ArcticDaisy.java
+++ src/Plants/Producers/ArcticDaisy.java
@@ -1,5 +1,23 @@
 package src.Plants.Producers;
 
-public class ArcticDaisy {
+/**
+ * class Artic Daisy
+ *
+ * @author Avi D.
+ * @version 5/14/2025
+ */
+public class ArcticDaisy extends Plant
+{
+    private static final int INITIAL_MASS = 0.35;  //individual ArcticDaisy dry mass in grams
+    private static final double GROWTH_RATE = 20.0;  //miligrams per gram per day
     
+    /**
+     * generates all of the ArcticDaisies
+     * 
+     */
+    public ArcticDaisy( )
+    {
+        // acres * individual/acre * grams/individual = grams
+        super( INITIAL_MASS, GROWTH_RATE );
+    }
 }
