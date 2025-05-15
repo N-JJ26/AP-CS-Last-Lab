06d2480bedabceba07e9ef9aa81a5644d5ad3caf
austinbenedicto <austinbenedicto271@gmail.com>
2025-05-15 09:24:24 -0400

    added java doc

--- src/Runner.java
+++ src/Runner.java
@@ -2,6 +2,12 @@ package src;
 
 import src.Animal.Animal;
 
+/**
+ * Runner class serves as the entry point for the simulation program.
+ * 
+ * @author Austin Benedicto, Nate Johnson, Avi Dasgupta
+ * @version 5/14/2025
+ */
 public class Runner
 {
   Animal[][] animals = new Animal[100][100];
