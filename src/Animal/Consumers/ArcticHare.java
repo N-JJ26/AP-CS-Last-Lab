597d38c8ac6520fd39fba4480a7204df3a0751cf
austinbenedicto <austinbenedicto271@gmail.com>
2025-05-16 13:14:02 -0400

    changed articHare a little to be more like Caribou

--- src/Animal/Consumers/ArcticHare.java
+++ src/Animal/Consumers/ArcticHare.java
@@ -11,6 +11,9 @@ public class ArcticHare extends Animal implements Herbivore
     private static final int WEANING_AGE = 120;
     private static final int ONSET_FERT_MALE = 680;
     private static final double MAX_LIFE_SPAN = 14.0; // in years
+    privaet static final int GRAMS_PER_DAY = 0; //TODO: put in constants
+
+    private boolean hasEaten;
 
     /**
      * Creates a new ArcticHare object via Animal().
@@ -18,23 +21,29 @@ public class ArcticHare extends Animal implements Herbivore
     public ArcticHare()
     {
         super(BODY_MASS_NEW_BORN, BODY_MASS_ADULT, WEANING_AGE, ONSET_FERT_MALE, MAX_LIFE_SPAN);
+        hasEaten = false;
     }
 
     /**
      * Simulates an ArcticHare eating another plant.
      */
-    public void eat(Plant p) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'eat'");
-    }
+     /**
+     * Simulates a Caribou eating another plant.
+     */
+    public void eat( Plant p )
+    {
+        p.consumed( GRAMS_PER_DAY );
 
+        hasEaten = true;
+    }
+    
     /**
      * Returns whether the animal isHungry
      * 
      * @return true if the animal is hungry, false otherwise.
      */
-    public boolean isHungry() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isHungry'");
-    }    
+    public boolean isHungry()
+    {
+        return hasEaten;
+    }
 }
