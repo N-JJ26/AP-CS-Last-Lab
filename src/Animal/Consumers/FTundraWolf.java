4384eaebef5dff15f84009e662089dd7ab13e40c
austinbenedicto <austinbenedicto271@gmail.com>
2025-05-16 12:45:43 -0400

    finished reproduce with for this class added

--- src/Animal/Consumers/FTundraWolf.java
+++ src/Animal/Consumers/FTundraWolf.java
@@ -5,12 +5,20 @@ import src.Animal.Female;
 
 public class FTundraWolf extends TundraWolf implements Female
 {
+//TODO: ADD IN CONSTANT FOR MAX LITTER SIZE AND AVG LITTER SIZE
+    private boolean pregnant;
+    private int gestationCount;
+    private int totalLitters;
+    private static final int MAX_LITTER = 0;
+    private static final int AVG_LITTER = 0;
+    private int totalBorn;
+    private Animal[] litter;
 
     public FTundraWolf()
     {
         super();
     }
-    
+
    public boolean reproduceWith( Animal male )
     {
         if(!this.isAlive() || !this.isAdult() || !this.isPregnant())
@@ -20,7 +28,21 @@ public class FTundraWolf extends TundraWolf implements Female
         if(male == null || !male.isAlive() || male.isAlive() || male instanceof Female || !(male instanceof Caribou))
             return false;
 
-        return false;
+        pregnant = true;
+        gestationCount = 0;
+
+        int size = 0;
+
+        if(totalLitters == 0)
+            size = (int)((MAX_LITTER + 1) * Math.random());
+        else if(1.0 * totalBorn / totalLitters < AVG_LITTER)
+            size = (int)((MAX_LITTER - AVG_LITTER) * Math.random() + AVG_LITTER);
+        else
+            size = (int)(AVG_LITTER * Math.random());
+        
+        litter = new Animal[size];
+        totalLitters++;
+        return pregnant;
     }
 
     @Override
