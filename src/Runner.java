df66d25042859b07eb1eb481561cf6ff92925e04
austinbenedicto <austinbenedicto271@gmail.com>
2025-05-20 11:35:17 -0400

    changed maybe

--- src/Runner.java
+++ src/Runner.java
@@ -2,8 +2,10 @@ package src;
 
 import src.Animal.Consumers.ArcticFox;
 
-public class Runner {
-    public static void main() {
+public class Runner
+{
+    public static void main()
+    {
         
     }
 }
