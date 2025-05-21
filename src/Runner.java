0e8b4fdc68e0ae3f3cf42ef6624d9b147b127016
Avi-Dasgupta <avidasgupta7@icloud.com>
2025-05-21 18:27:54 -0400

    noodling with getting the Caribou to eat

--- src/Runner.java
+++ src/Runner.java
@@ -6,24 +6,17 @@ import src.Plants.Producers.*;
 public class Runner {
   public static void main( String[] args ) {
     /* DECLARATIONS HERE */
-    ArcticWillow plant1 = new ArcticWillow();
-    CaribouMoss plant2 = new CaribouMoss();
-    ArcticDaisy plant3 = new ArcticDaisy(); 
+    Caribou car  = new Caribou( 1000 );
+    FCaribou far  = new FCaribou();
+    CaribouMoss moss = new CaribouMoss();
+    
 
     /* TEST CODE HERE */
-    System.out.println( "Willow: \n" + plant1.getTotalMass() );
-    for( int i = 0; i < 10; i++ )
-      plant1.grow();
-    System.out.println( plant1.getTotalMass() );
+    
+    System.out.println( car.isHungry() );
+    car.eat(moss);
+    System.out.println( car.isHungry() );
+    
 
-    System.out.println( "Moss: \n" + plant2.getTotalMass() );
-    for( int i = 0; i < 10; i++ )
-      plant2.grow();
-    System.out.println( plant2.getTotalMass() );
-
-    System.out.println( "Daisy: \n" + plant3.getTotalMass() );
-    for( int i = 0; i < 10; i++ )
-      plant3.grow();
-    System.out.println( plant3.getTotalMass() );
     }
 }
