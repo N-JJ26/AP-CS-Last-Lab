29a687c3b9012275784b5ce06d405274fa8acd16
N-JJ26 <nathanielj613@gmail.com>
2025-05-28 11:14:12 -0400

    working with degroat examples

--- src/Runner.java
+++ src/Runner.java
@@ -12,6 +12,16 @@ public class Runner {
     private static final int NUM_PLANTS = 3;
     private static final int NUM_ANIMALS = 5;
 
+    private static final int DAISY = 0;
+    private static final int WILLOW = 1;
+    private static final int MOSS = 2;
+
+    private static final int FOX = 0;
+    private static final int HARE = 1;
+    private static final int CARIBOU = 2;
+    private static final int POLAR_BEAR = 3;
+    private static final int WOLF = 4;
+
     private static Plant[] plants = new Plant[NUM_PLANTS];
     private static Animal[][] animals = new Animal[NUM_ANIMALS][];
 
@@ -23,36 +33,36 @@ public class Runner {
 
     private static void beginningSequence()
     {
-        System.out.print("Enter the acres of each plant:\nArctic Daisies = ");
-        plants[0] = new ArcticDaisy(in.nextInt());
+        System.out.print("Plants (acres):\nArctic Daisies = ");
+        plants[DAISY] = new ArcticDaisy(in.nextInt());
         in.next();
 
         System.out.print("Arctic Willow = ");
-        plants[1] = new ArcticWillow(in.nextInt());
+        plants[WILLOW] = new ArcticWillow(in.nextInt());
         in.next();
 
         System.out.print("Caribou Moss = ");
-        plants[2] = new CaribouMoss(in.nextInt());
+        plants[MOSS] = new CaribouMoss(in.nextInt());
         in.next();
 
         System.out.print("Animals (initial number of adults):\nArctic Foxes = ");
-        animals[0] = new ArcticFox[in.nextInt()];
+        animals[FOX] = new ArcticFox[in.nextInt()];
         in.next();
 
         System.out.print("Arctic Hares = ");
-        animals[1] = new ArcticHare[in.nextInt()];
+        animals[HARE] = new ArcticHare[in.nextInt()];
         in.next();
 
         System.out.print("Caribous = ");
-        animals[2] = new Caribou[in.nextInt()];
+        animals[CARIBOU] = new Caribou[in.nextInt()];
         in.next();
 
         System.out.print("Polar Bears = ");
-        animals[3] = new PolarBear[in.nextInt()];
+        animals[POLAR_BEAR] = new PolarBear[in.nextInt()];
         in.next();
 
         System.out.print("Tundra Wolves = ");
-        animals[4] = new TundraWolf[in.nextInt()];
+        animals[WOLF] = new TundraWolf[in.nextInt()];
         in.next();
     }
 
